a666ce61da905d538222476ccc5cdfe0d3f599c7
Alvin <akhuu92@gmail.com>
2019-05-24 12:31:35 -0700

    First commit for Calculator web app

---
+++ calculator.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+//information POSTed through HTTP Request
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/bmiCalculator", function (req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+//num1 and num2 are the input's "name"
+app.post("/", function (req, res) {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+
+    res.send("The result of the calculation is " + result);
+});
+
+app.post("/bmiCalculator", function (req, res) {
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height * weight);
+
+    res.send("Your BMI is " + bmi);
+})
+
+
+app.listen(3000, function () {
+    console.log("Server is listening on port 3000");
+});
